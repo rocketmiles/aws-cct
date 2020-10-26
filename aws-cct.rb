a1509794411823409c292bef274ef7dcb2da255e
goreleaserbot <goreleaser@carlosbecker.com>
2020-10-26 21:03:06 +0000

    Brew formula update for aws-cct version v1.0.0

--- aws-cct.rb
+++ aws-cct.rb
@@ -2,16 +2,16 @@
 class AwsCct < Formula
   desc "AWS Cost Comparison Tool"
   homepage "https://github.com/rocketmiles/aws-cct"
-  version "0.3.2"
+  version "1.0.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/rocketmiles/aws-cct/releases/download/v0.3.2/aws-cct_0.3.2_Darwin_x86_64.tar.gz"
-    sha256 "a968eb95942b0a2dcb20cb69ab9a5c29301cb12d9f1a2f7297d46f67c20b8888"
+    url "https://github.com/rocketmiles/aws-cct/releases/download/v1.0.0/aws-cct_1.0.0_Darwin_x86_64.tar.gz"
+    sha256 "50d62e662267ad65192e2b57e40b38dc4d7ad784681ef8f8309869b820b491ca"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/rocketmiles/aws-cct/releases/download/v0.3.2/aws-cct_0.3.2_Linux_x86_64.tar.gz"
-      sha256 "c94804c0eb7ff4e710e76dbad3d3f7b210fc8aaeda243d0225ec6484fb89868c"
+      url "https://github.com/rocketmiles/aws-cct/releases/download/v1.0.0/aws-cct_1.0.0_Linux_x86_64.tar.gz"
+      sha256 "1ac72a1beb71cec7c7cef64e2d92249f339d4e0c694cbba701a910d6bea1f852"
     end
   end
 
